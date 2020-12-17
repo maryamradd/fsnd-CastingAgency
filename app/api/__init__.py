1d2c2c7a030df33c05ab964285f9c4aeb1eb2a98
maryamradd <maryam.h.alraddadi@gmail.com>
2020-12-17 19:23:19 +0300

    static files path fix

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,13 +5,13 @@ from flask import Blueprint, send_from_directory
 from flask_cors import CORS
 
 # client app blueprint to serve client's static files
-client_app = Blueprint("client_app", __name__, static_folder=os.path.join(os.getcwd(), 'frontend/build'))
+client_app = Blueprint("client_app", __name__,static_url_path='',  static_folder=os.path.join(os.getcwd(), 'frontend/build/static'), template_folder=os.path.join(os.getcwd(), 'frontend/build'))
 CORS(client_app, resources={r"/*": {"origins": "*"}})
 
 # serve the react static files
 @client_app.route('/')
 def serve():
-    return send_from_directory(client_app.static_folder, 'index.html')
+    return send_from_directory(client_app.template_folder, 'index.html')
 
 # api blueprint
 api = Blueprint("api", __name__)
