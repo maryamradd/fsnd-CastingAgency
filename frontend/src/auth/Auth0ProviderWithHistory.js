2444520d1191b7f41bea0dc6d9841d72acf31cd8
maryamradd <maryam.h.alraddadi@gmail.com>
2020-12-23 00:32:18 +0300

    remove unused router history
    this component is not wraped under Router so it doesnt have access to history,
    used window.location instead

--- frontend/src/auth/Auth0ProviderWithHistory.js
+++ frontend/src/auth/Auth0ProviderWithHistory.js
@@ -1,17 +1,13 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
 import {Auth0Provider} from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({children}) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
-  const history = useHistory();
 
   const onRedirectCallback = (appState) => {
-    // Use the router's history module to replace the url
-    //history.replace(appState?.returnTo || window.location.pathname);
-    history.push(process.env.REACT_APP_URL);
+    window.location.href = process.env.REACT_APP_URL;
   };
 
   return (
